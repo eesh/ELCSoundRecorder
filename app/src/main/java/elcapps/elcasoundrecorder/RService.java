2459e32f4d48ef195618ac8119cb1c4e134111e7
Eesh <eeshlikhith@gmail.com>
2015-11-16 22:35:57 +0530

    - Instant wav saving instead of post processing.

--- app/src/main/java/elcapps/elcasoundrecorder/RService.java
+++ app/src/main/java/elcapps/elcasoundrecorder/RService.java
@@ -21,6 +21,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class RService extends Service {
 
@@ -79,7 +80,7 @@ public class RService extends Service {
             log("stopSelf() called at startRecording()");
             stopSelf();
         }
-        log("startRecording() recordingMode:"+ recordingMode);
+        log("startRecording() recordingMode:" + recordingMode);
         if(recordingMode == 2) {
             recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNELS, RECORDER_ENCODING, bufferSize);
             isRecording = true;
@@ -119,6 +120,88 @@ public class RService extends Service {
         }
     }
 
+
+    private void writeToFile() {
+        fileName = getFolderPath() + "recording " + fileNumber + ".wav";
+
+        byte sData[] = new byte[bufferSize];
+        BufferedOutputStream os = null;
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(fileName, true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        assert fos != null;
+        os = new BufferedOutputStream(fos);
+
+        long totalAudioLen = 0;
+        long totalDataLen = 0;
+        long longSampleRate = SAMPLE_RATE;
+        int channels = 1;
+        long byteRate = 16 * SAMPLE_RATE * channels/8;
+
+        try {
+            WriteWaveFileHeader(fos, totalAudioLen, totalDataLen,
+                    longSampleRate, channels, byteRate);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        while (isRecording) {
+            int gain = 1;
+            int result = recorder.read(sData, 0, bufferSize);
+            try {
+                if(result > 0) {
+                    for (int i = 0; i < result; ++i) {
+                        long temp;
+                        if((long)sData[i] != 0) {
+                            temp = (long) sData[i] * gain;
+                        } else {
+                            temp = (long) sData[i];
+                        }
+                        if (temp > 32767) { temp = 32767;}
+                        if (temp <- 32768) { temp = -32768;}
+                        sData[i] = (byte)temp;
+                    }
+                    assert os != null;
+                    os.write(sData, 0, result);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        try {
+            assert os != null;
+            os.close();
+            FileInputStream in = new FileInputStream(fileName);
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36; // + 36;
+            reWriteWaveFileHeader(fos, totalAudioLen, totalDataLen,
+                    longSampleRate, channels, byteRate);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(!paused) {
+            if(getActive()) {
+                log("Sending broadcast");
+                sendBroadcast(new Intent("elcarecorder.refresh"));
+            }
+            log("stopSelf() called at writeToFile");
+            stopSelf();
+        }
+
+    }
+
+
+
+
+
+    /*
 
     private void writeToFile() {
 
@@ -171,6 +254,8 @@ public class RService extends Service {
         }
 
     }
+
+    */
 
     private void copyWaveFile(String inFilename,String outFilename){
         FileInputStream in = null;
@@ -276,6 +361,64 @@ public class RService extends Service {
         header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
 
         out.write(header, 0, 44);
+    }
+
+    private void reWriteWaveFileHeader(FileOutputStream out, long totalAudioLen,
+            long totalDataLen, long longSampleRate, int channels,
+            long byteRate) throws IOException {
+
+        log("Writing wav header: " + out.getFD().toString());
+        byte[] header = new byte[44];
+
+        header[0] = 'R';  // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f';  // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1;  // format = 1
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (longSampleRate & 0xff);
+        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8);  // block align
+        header[33] = 0;
+        header[34] = 16;  // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+        RandomAccessFile file = new RandomAccessFile(fileName,"rw");
+        file.seek(0);
+        file.write(header);
+        file.close();
     }
 
 
